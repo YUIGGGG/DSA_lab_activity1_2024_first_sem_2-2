8f58ee660700669bddd5ea52a3a899fb593f5435
earlc <earlclyde197975@gmail.com>
2024-09-25 00:30:43 +0800

    exercise 1 done, draft only

--- lab_act1.py
+++ lab_act1.py
@@ -1,6 +1,11 @@
 temp = float(input("Input a temperature."))
 conv_type = input("Celsius to Fahrenheit or Fahrenheit to Celsius")
 
-if conv_type == ("Celsius" or "Cel" or "c"):
+if conv_type in ("Celsius" or "Cel" or "c"):
     answer = (9/5 * temp) - 32
     print(answer)
+elif conv_type in ("Fahrenheit" or "F"):
+    answer = (temp - 32) * 5/9
+    print(answer)
+else:
+    print("What you doin', that ain't the right input.")
