073ad4ec08327a90181ae2dbec8de08eb2ac8408
earlc <earlclyde197975@gmail.com>
2024-09-26 23:34:35 +0800

    done lab 3

--- lab_act3.py
+++ lab_act3.py
@@ -4,6 +4,13 @@ rows = int(input("Number of rows"))
 for n in range(0,rows):
     for i in range(0, rows - n - 1):
         print (" ", end="")
-    for j in range(0, i + 1):
+    for j in range(0, n + 1):
+        print("* ", end="")
+    print()
+
+for n in range(0, rows - 1):
+    for j in range(0, n + 1):
+        print(" ", end="")
+    for i in range(0, rows - n - 1):
         print("* ", end="")
     print()
