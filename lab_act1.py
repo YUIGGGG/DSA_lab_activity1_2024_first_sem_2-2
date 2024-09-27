6d7a083cea9cb05ac687b4701f84b256db588256
Earl Clyde Bañez <earlclyde197975@gmail.com>
2024-09-27 17:23:59 +0800

    lab act 1 donee

--- lab_act1.py
+++ lab_act1.py
@@ -1,11 +1,12 @@
-temp = float(input("Input a temperature."))
-conv_type = input("Celsius to Fahrenheit or Fahrenheit to Celsius")
+conv_type = input("Celsius to Fahrenheit or Fahrenheit to Celsius?\n"
+                  "(Input 'F' for Fahrenheit or 'C' for Celsius): ").strip().lower()
+temp = float(input("Input a temperature: "))
 
-if conv_type in ("Celsius" or "Cel" or "c"):
-    answer = (9/5 * temp) - 32
-    print(answer)
-elif conv_type in ("Fahrenheit" or "F"):
-    answer = (temp - 32) * 5/9
-    print(answer)
+if conv_type in ("c", "celsius"):
+    answer = (temp * 9/5) + 32  # Correct formula for Celsius to Fahrenheit
+    print(f"{temp}°C is {answer}°F.")
+elif conv_type in ("f", "fahrenheit"):
+    answer = (temp - 32) * 5/9  # Correct formula for Fahrenheit to Celsius
+    print(f"{temp}°F is {answer}°C.")
 else:
-    print("What you doin', that ain't the right input.")
+    print("What you doing? That ain't the right input.")
