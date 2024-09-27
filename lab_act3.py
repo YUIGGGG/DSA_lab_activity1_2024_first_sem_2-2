b80c89fbadae22ed5b95aac848038b4007fc7c3d
Earl Clyde Bañez <earlclyde197975@gmail.com>
2024-09-27 17:55:18 +0800

    lab act 3 donee

--- lab_act3.py
+++ lab_act3.py
@@ -1,16 +1,17 @@
-rows = int(input("Number of rows"))
+rows = int(input("Number of rows: "))
 
-
-for n in range(0,rows):
-    for i in range(0, rows - n - 1):
-        print (" ", end="")
-    for j in range(0, n + 1):
+# Upper part of the diamond
+for n in range(rows):
+    for i in range(rows - n - 1):
+        print(" ", end="")
+    for j in range(n + 1):
         print("* ", end="")
     print()
 
-for n in range(0, rows - 1):
-    for j in range(0, n + 1):
+# Lower part of the diamond
+for n in range(rows - 1):
+    for j in range(n + 1):
         print(" ", end="")
-    for i in range(0, rows - n - 1):
+    for i in range(rows - n - 1):
         print("* ", end="")
     print()
